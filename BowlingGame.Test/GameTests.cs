f1cb5e22c95599598ae88f34f1eb48b543646231
Mark Evans <mark.evans@microlise.com>
2020-01-12 14:13:36 +0000

    Add project files.

---
+++ BowlingGame.Test/GameTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingGame.Test
+{
+    public class GameTests
+    {
+        private Game _game;
+
+        [Test]
+        public void Test_Game_Constructor_Successful()
+        {
+            Assert.DoesNotThrow(new TestDelegate(CreateNewGame));
+        }
+
+        private void CreateNewGame()
+        {
+            _game = new Game();
+        }
+    }
+}
